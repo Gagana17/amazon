37bf5fc03212c305f95aa8b74c5bc3cf8e57ed44
Gagana17 <gaganatcc@gmail.com>
2023-03-06 21:49:01 +0530

    design5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,13 +8,15 @@ function App() {
       </header>
       <main>
         <h1>Featured product</h1>
+        <div className="products">
         {data.products.map((product) => (
-          <div>
+          <div className="product" key={product.slug}>
             <img src={product.image} alt={product.name} />
             <p>{product.name}</p>
             <p>{product.quantity}</p>
           </div>
         ))}
+        </div>
       </main>
     </div>
   );
