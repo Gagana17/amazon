0fdb4743f9f39ce0700e79e1c1bbc1c0f36641ae
Gagana17 <gaganatcc@gmail.com>
2023-03-06 21:36:22 +0530

    vi 04

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">Lankan flavor pvt</a>
       </header>
-      <main>list product</main>
+      <main>
+        <h1>Featured product</h1>
+        {data.products.map((product) => (
+          <div>
+            <img src={product.image} alt={product.name} />
+            <p>{product.name}</p>
+            <p>{product.quantity}</p>
+          </div>
+        ))}
+      </main>
     </div>
   );
 }
