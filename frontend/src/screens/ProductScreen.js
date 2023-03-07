7268e153966b6e52648caec40202f04e072a7f38
Gagana17 <gaganatcc@gmail.com>
2023-03-07 14:57:31 +0530

    1

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+function ProductScreen() {
+  const params = useParams();
+  const { slug } = params;
+  return (
+    <div>
+      <h1>{slug}</h1>
+    </div>
+  );
+}
+
+export default ProductScreen;
