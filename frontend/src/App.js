219211c09d4ae8b33ed4e426f3ef42b79c3c5664
Gagana17 <gaganatcc@gmail.com>
2023-03-07 14:03:09 +0530

    1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import data from './data';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from ' react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 
 function App() {
