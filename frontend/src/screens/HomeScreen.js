6a0260003351c4df13f4f1c821d202dc10f53075
Gagana17 <gaganatcc@gmail.com>
2023-03-07 15:51:41 +0530

    b

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,5 +1,8 @@
 /* eslint-disable no-undef */
 /* eslint-disable jsx-a11y/anchor-is-valid */
+
+import { Link } from 'react-router-dom';
+
 /* eslint-disable react/jsx-no-comment-textnodes */
 function HomeScreen() {
   return (
@@ -10,13 +13,13 @@ function HomeScreen() {
         {data.products.map((product) => (
           <div className="product" key={product.slug}>
             // eslint-disable-next-line jsx-a11y/anchor-is-valid
-            <a herf={`/product/ ${product.slug}`}>
+            <Link to={`/product/ ${product.slug}`}>
               <img src={product.image} alt={product.name} />
-            </a>
+            </Link>
             <div classname="product-info">
-              <a herf={`/product/ ${product.slug}`}>
+              <Link to={`/product/ ${product.slug}`}>
                 <p>{product.name}</p>
-              </a>
+              </Link>
               <p>
                 <strong>{product.quantity}</strong>{' '}
               </p>
