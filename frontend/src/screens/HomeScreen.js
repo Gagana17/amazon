98d331027f7c9e60f76a58184efb9c531c55a521
Gagana17 <gaganatcc@gmail.com>
2023-03-07 15:43:57 +0530

    m

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,10 +1,15 @@
+/* eslint-disable no-undef */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable react/jsx-no-comment-textnodes */
 function HomeScreen() {
   return (
     <div>
       <h1>Featured product</h1>
       <div className="products">
+        // eslint-disable-next-line no-undef
         {data.products.map((product) => (
           <div className="product" key={product.slug}>
+            // eslint-disable-next-line jsx-a11y/anchor-is-valid
             <a herf={`/product/ ${product.slug}`}>
               <img src={product.image} alt={product.name} />
             </a>
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 function ProductScreen() {
   const params = useParams();
   const { slug } = params;
