eb5aab680a592083b2f02c34c60b009b4e338bee
Gagana17 <gaganatcc@gmail.com>
2023-03-07 14:10:39 +0530

    a

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,26 +14,6 @@ function App() {
           <Routes>
             <Route path="/" element={<HomeScreen />} />
           </Routes>
-          <h1>Featured product</h1>
-          <div className="products">
-            {data.products.map((product) => (
-              <div className="product" key={product.slug}>
-                <a herf={`/product/ ${product.slug}`}>
-                  <img src={product.image} alt={product.name} />
-                </a>
-                <div classname="product-info">
-                  <a herf={`/product/ ${product.slug}`}>
-                    <p>{product.name}</p>
-                  </a>
-                  <p>
-                    <strong>{product.quantity}</strong>{' '}
-                  </p>
-
-                  <button>More detais</button>
-                </div>
-              </div>
-            ))}
-          </div>
         </main>
       </div>
     </BrowserRouter>
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,4 +1,29 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable no-undef */
 function HomeScreen() {
-  return <div>Home HomeScreen</div>;
+  return (
+    <div>
+      <h1>Featured product</h1>
+      <div className="products">
+        {data.products.map((product) => (
+          <div className="product" key={product.slug}>
+            <a herf={`/product/ ${product.slug}`}>
+              <img src={product.image} alt={product.name} />
+            </a>
+            <div classname="product-info">
+              <a herf={`/product/ ${product.slug}`}>
+                <p>{product.name}</p>
+              </a>
+              <p>
+                <strong>{product.quantity}</strong>{' '}
+              </p>
+
+              <button>More detais</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 export default HomeScreen;
