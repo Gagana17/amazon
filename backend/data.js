ed82faaa639052196d04defa2fcd1000d782266e
Gagana17 <gaganatcc@gmail.com>
2023-03-07 23:17:55 +0530

    1

---
+++ backend/data.js
@@ -0,0 +1,38 @@
+const data = {
+  products: [
+    {
+      name: 'Turmeric',
+      category: 'Raw materials',
+      image: '/images/p1.jpg', //679px * 829px
+      quantity: '1000kg',
+      brand: 'Turmeric powder',
+      description: 'high quality materials',
+    },
+    {
+      name: 'Cinnamon',
+      category: 'Raw materials',
+      image: '/images/p2.jpg', //679px * 829px
+      quantity: '3000kg',
+      brand: 'Lankan Cinnamon',
+      description: 'high quality materials',
+    },
+    {
+      name: 'Ginger',
+      category: 'Raw materials',
+      image: '/images/p3.jpg', //679px * 829px
+      quantity: '2000kg',
+      brand: 'Ginger',
+      description: 'high quality materials',
+    },
+    {
+      name: 'Pepper',
+      category: 'Raw materials',
+      image: '/images/p4.jpg', //679px * 829px
+      quantity: '3000kg',
+      brand: 'Pepper Powder',
+      description: 'high quality materials',
+    },
+  ],
+};
+
+export default data;
