0e2def1355b471c6e9746d15cc177031211da80e
Gagana17 <gaganatcc@gmail.com>
2023-03-07 17:15:19 +0530

    1

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,5 +1,6 @@
+import { useParams } from 'react-router-dom';
+
 function ProductScreen() {
-  // eslint-disable-next-line no-undef
   const params = useParams();
   const { slug } = params;
   return (
