7ff0ded3d916d33a2edcd66375c7570c2cbb5a19
Gagana17 <gaganatcc@gmail.com>
2023-03-08 23:22:35 +0530

    1

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 //import data from '../data';
 import axios from 'axios';
+
 function HomeScreen() {
   const [products, setProducts] = useState([]);
   useEffect(() => {
@@ -15,7 +16,7 @@ function HomeScreen() {
     <div>
       <h1>Featured product</h1>
       <div className="products">
-        {products.products.map((product) => (
+        {products.map((product) => (
           <div className="product" key={product.slug}>
             <Link to={`/product/ ${product.slug}`}>
               <img src={product.image} alt={product.name} />
