6c074b39a0a97ee081cfac255f8b668b43e548a4
Gagana17 <gaganatcc@gmail.com>
2023-03-10 22:52:20 +0530

    lp

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,15 +1,42 @@
+/* eslint-disable default-case */
+// eslint-disable-next-line no-unused-vars
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 //import data from '../data';
 import axios from 'axios';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCES':
+      return { ...state, products: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+      // eslint-disable-next-line no-labels, no-unreachable, no-unused-labels
+      deafault: return state;
+  }
+};
+
 function HomeScreen() {
-  const [products, setProducts] = useState([]);
+  // eslint-disable-next-line no-undef, no-unused-vars
+  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
+    products: [],
+    loading: true,
+    error: '',
+  });
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get('/api/products');
-      setProducts(result.data);
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const result = await axios.create('/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+      }
+      //setProducts(result.data);
     };
+
     fetchData();
   }, []);
   return (
